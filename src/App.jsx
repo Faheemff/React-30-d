0c424d7f82e5a1345e1da25c887f2106740cc4b6
Faheemff <mfaheemweb@gmail.com>
2024-10-20 09:36:06 -0700

    initial stage 0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>heelo
+            
+        </h1>
+    </div>
+  )
+}
+
+export default App
