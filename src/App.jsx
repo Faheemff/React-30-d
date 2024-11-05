f0ebf9b00b4b908e1274c3970ba329bb563151f1
Faheemff <mfaheemweb@gmail.com>
2024-11-05 22:59:46 +0500

    add more files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Style from './HeadingCompen/Style'
 
 const App = () => {
   return (
     <div>
-        <h1>heelo
-            
-        </h1>
+      <h1>heelow</h1>
+      <Style />
     </div>
   )
 }
---
+++ src/HeadingCompen/Style.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Style = () => {
+  return (
+    <div>
+        {/* inline css */}
+        <div><h1 style={{color:"red", height: "100px", background:"green",  display:"flex", alignItems:"center" , justifyContent:"center" , textAlign:"center"}}>Hello</h1></div>
+    </div>
+  )
+}
+
+export default Style
